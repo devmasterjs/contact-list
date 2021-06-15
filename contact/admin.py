eb58c3dd47bb62e1b1b43481ebb5750f94117fb6
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-06-14 23:35:47 -0300

    feat: added Contact model

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(Contact, ContactAdmin)
