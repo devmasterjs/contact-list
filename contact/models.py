6442148cb8d132a4a7c0cda5c5a8938edfae4110
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-06-14 22:53:55 -0300

    feat: added .editorconfig file

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import pgettext_lazy as _
 
-# Create your models here.
+
+class Contact(models.Model):
+  code = models.CharField(_("code"), max_length=50)
+  created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+  updated_at = models.DateTimeField(_("updated at"), auto_now=True)
